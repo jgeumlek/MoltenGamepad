7c0ad1e886901f83a8ef70ed796a91f8fc43d3d8
Joseph Geumlek <josephgeumlek@gmail.com>
2016-02-13 12:46:41 -0800

    Add ability to move all devices, add ability to unassign a device.

--- source/commands/move.cpp
+++ source/commands/move.cpp
@@ -6,7 +6,7 @@
 #include "../parser.h"
 
 
-#define MOVE_USAGE "USAGE:\n\tmove <device> to <slot>"
+#define MOVE_USAGE "USAGE:\n\tmove <device> to <slot>\n\t\"all\" can be used to refer to all devices\n\t\"nothing\" can be used as a slot name to remove from all slots"
 int do_move(moltengamepad* mg, std::vector<token> &command) {
   if (command.size() < 4) {std::cout<<MOVE_USAGE<<std::endl;return -1;}
   if (command.at(2).value != "to") {std::cout<<MOVE_USAGE<<std::endl;return -1;}
@@ -14,9 +14,15 @@ int do_move(moltengamepad* mg, std::vector<token> &command) {
   std::string slotname = command.at(3).value;
   std::shared_ptr<input_source> dev = mg->find_device(devname.c_str());
   virtual_device* slot = mg->slots->find_slot(slotname);
-  if (!dev.get()) {std::cout << "device " << devname << " not found.\n"<<MOVE_USAGE << std::endl; return -1;};
-  if (!slot) {std::cout << "slot " << slotname << " not found.\n"<<MOVE_USAGE << std::endl; return -1;};
-  mg->slots->move_to_slot(dev.get(),slot);
+  if (!dev.get() && devname != "all") {std::cout << "device " << devname << " not found.\n"<<MOVE_USAGE << std::endl; return -1;};
+  if (!slot && slotname != "nothing") {std::cout << "slot " << slotname << " not found.\n"<<MOVE_USAGE << std::endl; return -1;};
+  if (devname != "all") {
+    mg->slots->move_to_slot(dev.get(),slot);
+  } else {
+    for (auto dev : mg->devices) {
+      mg->slots->move_to_slot(dev.get(),slot);
+    }
+  }
   return 0;
 }
   
