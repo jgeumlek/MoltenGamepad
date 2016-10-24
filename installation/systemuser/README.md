c9588107598c469503a10339d5caed714c05efda
Joseph Geumlek <josephgeumlek@gmail.com>
2016-10-23 21:21:45 -0700

    add installation files provided TIZ-EX1

---
+++ installation/systemuser/README.md
@@ -0,0 +1,3 @@
+# System Files
+
+A system-wide installation of MoltenGamepad can offer several benefits. Base devices will *always* be invisible to regular users without having to change permissions, and virtual devices will always be visible. Systemd can make sure a directory is available for MG's fifo, and start it automatically. Run `./install.sh` from this directory to deploy this setup.
---
+++ installation/systemuser/install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+# Only root can do anything useful here, so might as well...
+[[ $EUID -ne 0 ]] && exec sudo "$0"
+
+# Create system user and group for MG.
+useradd -r -U gamepad
+
+# Install the files contained in the repo.
+cd "$(dirname "$0")"
+install -Dm0644 profile-sdl2.sh /etc/profile.d/sdl2-gamecontroller.sh
+install -Dm0644 systemd.service /etc/systemd/system/moltengamepad.service 
+install -Dm0644 tmpfiles.conf /etc/tmpfiles.d/moltengamepad.conf
+install -Dm0644 udev.rules /etc/udev/rules.d/90-gamepad.rules
+
+# Reload the various services we have affected.
+udevadm control --reload
+systemd-tmpfiles --create
+systemctl daemon-reload
+systemctl enable moltengamepad.service
+
+# Tell the user we're done and what to do next.
+echo "Installation of system files complete. Install the MoltenGamepad binary"
+echo "as /usr/local/bin/moltengamepad and run systemctl start moltengamepad"
+echo "to start system-mode MoltenGamepad."
