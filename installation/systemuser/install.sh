aa501f9b6c88379562be9eec46f176f53a3430c4
Joseph Geumlek <josephgeumlek@gmail.com>
2017-01-21 21:23:40 -0800

    improve installation as system user for old udev

--- installation/systemuser/install.sh
+++ installation/systemuser/install.sh
@@ -24,6 +24,35 @@ rmmod uinput
 modprobe uinput
 systemctl enable moltengamepad.service
 
+# Try to detect a case where "TAG-=uaccess" is not supported.
+# This work-around uses "last_rule" which can lead to surprising behavior when writing other udev rules.
+# As a courtesy, alert the user in case they have interest in writing udev rules later.
+UDEV_VERSION=$(udevadm --version)
+if [[ "$?" -ne "0" ]]; then
+  UDEV_VERSION=""
+  echo "Could not detect udev version. If the version is less than 217, the uaccess tag cannot be removed."
+fi
+
+if [[ "$UDEV_VERSION" != "" && "$UDEV_VERSION" -ge "217" ]]; then
+  echo "Detected version of udev that supports removing tags."
+  echo ""
+fi
+
+if [[ "$UDEV_VERSION" != "" && "$UDEV_VERSION" -lt "217" ]]; then
+  echo "An old version of udev was detected! This version does not support removing the uaccess tag."
+  echo ""
+fi
+
+if [[ "$UDEV_VERSION" == "" || "$UDEV_VERSION" -lt "217" ]]; then
+  echo "If the uaccess tag is not removed, then hiding the original device nodes may fail in certain situations."
+  echo "To remedy this, please comment out the following line in /lib/udev/rules.d/70-uaccess.rules"
+  echo -e '\tSUBSYSTEM=="input", ENV{ID_INPUT_JOYSTICK}=="?*", TAG+="uaccess"'
+  echo ""
+  echo "A modified version can be placed at /etc/udev/rules.d/70-uaccess.rules to shadow the original file in /lib/."
+  echo "The provided script modify_uaccess_rules.sh will perfrom this for you."
+  echo ""
+fi
+
 # Tell the user we're done and what to do next.
 echo "Installation of system files complete. Install the MoltenGamepad binary"
 echo "as /usr/local/bin/moltengamepad and run systemctl start moltengamepad"
