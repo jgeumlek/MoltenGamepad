352911de15ac018550e50982a254e5edf1d1b9be
gitaen <gitaen@alenri.net>
2018-02-02 22:53:16 +0100

    allow use of axis in chord group translator (#50)
    * allow use of axis in chord group translator
    
    axis direction can be specified using + or -.
    
    examples:
    
    xpad.(start,leftright+) = chord(select)
    xpad.(start,leftright) = chord(select). Same thing as above
    
    xpad.(start,leftright-) = chord(mode)
    
    * use ABS_RANGE/2 as axis threshold on chord group translator

--- source/core/event_translators/group/simple_chord.cpp
+++ source/core/event_translators/group/simple_chord.cpp
@@ -10,9 +10,10 @@ simple_chord::simple_chord(std::vector<MGField>& fields) {
 bool simple_chord::set_mapped_events(const std::vector<source_event>& listened) {
   num_events = listened.size();
   for (auto ev : listened) {
-    if (ev.type != DEV_KEY)
+    if ((ev.type != DEV_KEY) && (ev.type != DEV_AXIS))
       return false;
     event_vals.push_back(ev.value);
+    event_thres.push_back(ev.type == DEV_KEY?0:ABS_RANGE/2);
   }
   return true;
 }
@@ -37,7 +38,7 @@ simple_chord::~simple_chord() {
 
 bool simple_chord::claim_event(int id, mg_ev event) {
   bool output = true;
-  event_vals[id] = event.value;
+  event_vals[id] = event.value > event_thres[id];
   for (int i = 0; i < event_vals.size(); i++) {
     output = output && (event_vals[i]);
   }
